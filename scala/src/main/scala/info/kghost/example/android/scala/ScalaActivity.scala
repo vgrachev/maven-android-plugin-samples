6b5fd27eb70ab51828c22c16c6fbc55eedc9fac3
Hugo Josefson <hugo@josefson.org>
2010-10-12 15:43:16 +0200

    Renamed to ScalaActivity, so it's easier to identify.

--- scala/src/main/scala/info/kghost/example/android/scala/ScalaActivity.scala
+++ scala/src/main/scala/info/kghost/example/android/scala/ScalaActivity.scala
@@ -3,7 +3,7 @@ package info.kghost.example.android.scala
 import android.app.Activity
 import android.os.Bundle
 
-class TestActivity extends Activity {
+class ScalaActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
