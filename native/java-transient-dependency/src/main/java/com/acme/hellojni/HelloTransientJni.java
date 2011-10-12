944c96c973fff7c04525cd9802119bf27aae7671
johan <johan@oden.(none)>
2011-10-12 13:44:48 -0400

    Sample for transiently pulling in native dependencies

--- native/java-transient-dependency/src/main/java/com/acme/hellojni/HelloTransientJni.java
+++ native/java-transient-dependency/src/main/java/com/acme/hellojni/HelloTransientJni.java
@@ -19,7 +19,7 @@ import android.app.Activity;
 import android.widget.TextView;
 import android.os.Bundle;
 
-public class HelloJni extends Activity
+public class HelloTransientJni extends Activity
 {
     /** Called when the activity is first created. */
     @Override
@@ -29,37 +29,11 @@ public class HelloJni extends Activity
 
         /* Create a TextView and set its content.
          * the text is retrieved by calling a native
-         * function.
+         * function on a transiently included native library.
          */
         TextView  tv = new TextView(this);
-        tv.setText( stringFromJNI() );
+        tv.setText( new HelloJni().stringFromJNI() );
         setContentView(tv);
     }
 
-    /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
-     * with this application.
-     */
-    public native String  stringFromJNI();
-
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-    public native String  unimplementedStringFromJNI();
-
-    /* this is used to load the 'native-code' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.acme.HelloJni/lib/libnative-code.so at
-     * installation time by the package manager.
-     */
-    static {
-        System.loadLibrary("native-code");
-    }
 }
